4461a7443f81de203a2291ca34ed6e5e59b8f8c1
Curtis Lane <clanelmt@gmail.com>
2020-09-17 16:07:45 -0400

    Updates

---
+++ precache-manifest.1b592b039f78f12d04a8ab416d063ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4ffad4aede3a809d832ded261d3c73",
+    "url": "/Web-Developer-Portfolio/index.html"
+  },
+  {
+    "revision": "2b1412c13724937e5d06",
+    "url": "/Web-Developer-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/Web-Developer-Portfolio/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Developer-Portfolio/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1412c13724937e5d06",
+    "url": "/Web-Developer-Portfolio/static/js/main.10d8fed2.chunk.js"
+  },
+  {
+    "revision": "8138856acdf1e8eb8322",
+    "url": "/Web-Developer-Portfolio/static/js/runtime-main.13bd0ee7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web-Developer-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
